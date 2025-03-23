7f0d594b034f2d4d27819558355ebdb411017134
Ghoen <152753975+Ghoen@users.noreply.github.com>
2025-03-23 17:42:37 -0600

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+// Navbar
+
 document.addEventListener('DOMContentLoaded', function() {
     const navWrapper = document.getElementById('nav-wrapper'); // Cambio a `nav-wrapper`
     const welcomeSection = document.getElementById('welcome-section');
@@ -14,4 +16,51 @@ document.addEventListener('DOMContentLoaded', function() {
     }, { threshold: 0.75 });
 
     observer.observe(welcomeSection);
+});
+
+// Modal
+
+document.addEventListener("DOMContentLoaded", () => {
+    const modal = document.getElementById("modal-container");
+    const modalImg = document.getElementById("modal-image");
+    const closeModal = document.querySelector(".close");
+    
+    document.querySelectorAll(".open-modal-btn").forEach(button => {
+        button.addEventListener("click", function() {
+        const imgSrc = this.getAttribute("data-image");
+        modalImg.src = imgSrc;
+        modal.style.display = "flex";
+    });
+    });
+
+    closeModal.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+
+    modal.addEventListener("click", (e) => {
+        if (e.target === modal) {
+        modal.style.display = "none";
+        }
+    });
+});
+
+// Language switcher
+
+document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.getElementById("cb3-8");
+
+    // Detectar la página actual
+    const isSpanish = window.location.pathname.includes("index_es");
+
+    // Marcar el checkbox si estamos en español
+    toggle.checked = !isSpanish;
+
+    // Redirigir cuando cambia el checkbox
+    toggle.addEventListener("change", () => {
+        if (toggle.checked) {
+            window.location.href = "index.html"; // Página en inglés
+        } else {
+            window.location.href = "index_es.html"; // Página en español
+        }
+    });
 });
